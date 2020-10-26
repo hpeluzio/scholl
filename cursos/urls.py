ec6b7096ce83b76118b9d1aafa03f5880737c730
Henrique Peluzio <hpeluzio@gmail.com>
2020-10-25 21:22:54 -0300

    APIView + Post

--- cursos/urls.py
+++ cursos/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import CursoAPIView, AvaliacaoAPIView
 
 urlpatterns = [
-  path('cursos/', CursoAPIView.as_views(), name='cursos')
-  path('avaliacoes/', AvaliacaoAPIView.as_views(), name='avaliacoes')
+  path('cursos/', CursoAPIView.as_view(), name='cursos'),
+  path('avaliacoes/', AvaliacaoAPIView.as_view(), name='avaliacoes'),
 ]
